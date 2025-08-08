02551df891c6ef85b5e1c4c28d9e758ca273a231
Queenjieun <ljejjk0623@gmail.com>
2025-08-08 22:15:35 +0900

    파이썬 파일들 backend 폴더에 추가

---
+++ Koring/backend/Mainpage.py
@@ -0,0 +1,29 @@
+from flask import Blueprint,render_template
+
+main_bp = Blueprint('main', __name__)
+
+
+@main_bp.route('/')
+def home():
+    return render_template('gyeongbokgung.html')
+
+@main_bp.route('/main')
+def main_page():
+    return render_template('gyeongbokgung.html')
+
+@main_bp.route('/integrated')
+def integrated_page():
+    return render_template('integrated.html')
+
+@main_bp.route('/reservation')
+def reservation_page():
+    return render_template('reservation.html')
+
+@main_bp.route('/restaurant')
+def restaurant_page():
+    return render_template('restaurant.html')
+
+@main_bp.route('/album')
+def album_page():
+    return render_template('album.html')
+
